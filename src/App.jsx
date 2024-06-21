cf636bc3e9bef2fe94a0ab154f6aef6894a462dd
marcovittoriozacca <zaccamarco01@gmail.com>
2024-06-21 13:52:59 +0200

    feat: Basic routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import Posts from "./pages/Posts"
+import SinglePost from "./pages/SinglePost"
+import NotFound from "./pages/NotFound"
+
 function App() {
 
   return (
     <>
-    
+    <BrowserRouter>
+      <Routes>
+
+        {/* home page route */}
+        <Route path="/" element={<HomePage/>}/>
+
+        {/* Not Found Route */}
+        <Route path="/*" element={<NotFound/>}/>
+
+        {/* complete posts list route */}
+        <Route path="/posts">
+          <Route index element={<Posts/>}/>
+          {/* single post route with slug param */}
+
+          <Route path=":slug">
+              <Route index element={<SinglePost/>}/>    
+          </Route>
+        </Route>
+
+      </Routes>    
+    </BrowserRouter>
     </>
   )
 }
